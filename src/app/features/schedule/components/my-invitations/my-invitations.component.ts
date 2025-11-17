8160fa3455727c40d8faccd1532a5e3edbff7bf2
Ako <akaki.vashakidze1@gmail.com>
2025-11-17 21:13:57 +0400

    adding tabs invitations page

--- src/app/features/schedule/components/my-invitations/my-invitations.component.ts
+++ src/app/features/schedule/components/my-invitations/my-invitations.component.ts
@@ -3,34 +3,66 @@ import { Component } from '@angular/core';
 import { InvitationsService } from '../../services/invitations.service';
 import { Invitation } from '../../../../interfaces/shared.interface';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-my-invitations',
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, MatTabsModule],
   templateUrl: './my-invitations.component.html',
   styleUrl: './my-invitations.component.scss'
 })
 export class MyInvitationsComponent {
   invitations!: Invitation[];
   constructor(private invitationService: InvitationsService) {
-    invitationService.GetInvitations(null, null, null, null, null, null, null).subscribe(result => {
+    this.getInvitations(0)
+  }
+
+  getInvitations(approved: number | null) {
+    this.invitationService.GetInvitations(null, null, approved, null, null, null, null).subscribe(result => {
       console.log('Invitations:', result);
       this.invitations = result.result.data;
     });
   }
-
-  acceptInvitation(invitation: string) {
-    // this.invitationService.respondToInvitation(invitation.id, true).subscribe(response => {
-    //   console.log('Accepted invitation response:', response);
-    //   // Optionally update the UI or invitations list here
-    // });
-    console.log(invitation)
+  approvedCheck(approved: number) {
+    switch (approved) {
+      case 0:
+        return 'Pending';
+      case 1:
+        return 'Accepted';
+      case -1:
+        return 'Declined';
+      default:
+        return 'Unknown';
+    } 
   }
 
-  declineInvitation(invitation: string) {
-    // this.invitationService.respondToInvitation(invitation.id, false).subscribe(response => {
-    //   console.log('Declined invitation response:', response);
-    //   // Optionally update the UI or invitations list here
-    // });
+  acceptInvitation(invitationId: string) {
+    this.invitationService.acceptInvitation(invitationId).subscribe(response => {
+      console.log('Accepted invitation response:', response);
+      // Optionally update the UI or invitations list here
+    });
+  }
+
+  declineInvitation(invitationId: string) {
+    this.invitationService.declineInvitation(invitationId).subscribe(response => {
+      console.log('Declined invitation response:', response);
+    });
   }   
+
+  onTabSwitched(event: any) {
+    console.log('Tab switched to index:', event.index);
+    switch (event.index) {
+      case 0:
+        this.getInvitations(0); 
+        break;
+      case 1:
+        this.getInvitations(1); 
+        break;
+      case 2:
+        this.getInvitations(-1);
+        break;
+      default:
+        this.getInvitations(null);
+    }
+  }
 }
--- src/app/features/schedule/services/invitations.service.ts
+++ src/app/features/schedule/services/invitations.service.ts
@@ -12,14 +12,22 @@ export class InvitationsService {
 
   GetInvitations(searchQuery: string | null, location:string | null, approved:number | null, active:number | null, weekday:string | null, specificDate:Date | null, urjent:number | null ): Observable<GenericResponse<Invitation[]>> {
     let body : {searchQuery?: string, location?: string, approved?: number, active?: number, weekday?: string, specificDate?: Date, urjent?: number} = {};
-    searchQuery ? body.searchQuery = searchQuery : null;
-    location ? body.location = location : null;
-    approved ? body.approved = approved : null;
-    active ? body.active = active : null;
-    weekday ? body.weekday = weekday : null;
-    specificDate ? body.specificDate = specificDate : null;  
-    urjent ? body.urjent = urjent : null;
+    searchQuery != null ? body.searchQuery = searchQuery : null;
+    location != null ? body.location = location : null;
+    approved != null ? body.approved = approved : null;
+    active != null ? body.active = active : null;
+    weekday != null ? body.weekday = weekday : null;
+    specificDate != null ? body.specificDate = specificDate : null;  
+    urjent != null ? body.urjent = urjent : null;
     return this.http.post<GenericResponse<Invitation[]>>(`/consoleApi/invitation/list`, body);
   }
+
+  acceptInvitation(invitationId: string): Observable<GenericResponse<any>> {
+    return this.http.post<GenericResponse<any>>(`/consoleApi/invitation/accept/${invitationId}`,{});
+  }
+
+  declineInvitation(invitationId: string): Observable<GenericResponse<any>> {
+    return this.http.post<GenericResponse<any>>(`/consoleApi/invitation/decline/${invitationId}`,{});
+  } 
 
 }
